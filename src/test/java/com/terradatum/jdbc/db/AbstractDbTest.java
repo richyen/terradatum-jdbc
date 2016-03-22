11948af280eeb7f0237320fdbaba60524831fdf5
G. Richard Bellamy <rbellamy@terradatum.com>
2016-03-22 12:04:13 -0700

    Move model-specific tests from JDBC to terradatum-db-model

---
+++ src/test/java/com/terradatum/jdbc/db/AbstractDbTest.java
@@ -0,0 +1,8 @@
+package com.terradatum.jdbc.db;
+
+/**
+ * @author rbellamy
+ * @date 3/20/16
+ */
+public class AbstractDbTest {
+}
---
+++ src/test/java/com/terradatum/jdbc/db/DbOperations.java
@@ -0,0 +1,8 @@
+package com.terradatum.jdbc.db;
+
+/**
+ * @author rbellamy@terradatum.com
+ * @date 3/18/16
+ */
+public class DbOperations {
+}
