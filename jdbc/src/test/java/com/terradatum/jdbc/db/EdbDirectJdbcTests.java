9f8a9c9e61d5cf4ceee49cc1a611d18defdabba9
G. Richard Bellamy <rbellamy@terradatum.com>
2016-04-13 08:12:53 -0700

    Consolidate direct JDBC unit tests
    Consolidate direct JDBC unit tests into separate files - this will help
    when discussing issues with the EDB JDBC drivers.

---
+++ jdbc/src/test/java/com/terradatum/jdbc/db/EdbDirectJdbcTests.java
@@ -0,0 +1,54 @@
+package com.terradatum.jdbc.db;
+
+import com.ninja_squad.dbsetup.DbSetup;
+import com.ninja_squad.dbsetup.DbSetupTracker;
+import com.ninja_squad.dbsetup.destination.DataSourceDestination;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.Array;
+import java.sql.SQLException;
+import java.sql.Struct;
+
+/**
+ * @author rbellamy@terradatum.com
+ * @date 4/12/16
+ */
+public class EdbDirectJdbcTests extends DirectJdbcTests {
+  private static final Logger LOGGER = LoggerFactory.getLogger(EdbDirectJdbcTests.class);
+  private static DbSetupTracker dbSetupTracker = new DbSetupTracker();
+
+  @Before
+  public void prepare() throws Exception {
+    assert ORACLE_DATA_SOURCE != null : "Datasource cannot be null";
+    DbSetup dbSetup = new DbSetup(new DataSourceDestination(ORACLE_DATA_SOURCE), super.setupDb());
+    LOGGER.debug(dbSetupTracker.toString());
+    dbSetupTracker.launchIfNecessary(dbSetup);
+  }
+
+  @Test
+  public void canGetParentByNameUsingDirectJdbc() throws SQLException {
+    dbSetupTracker.skipNextLaunch();
+    Struct parentStruct = getParentStructByNameUsingDirectJdbc(getEdbConnection());
+    Assert.assertNotNull(parentStruct);
+    Assert.assertTrue("The attribute is not an Struct", Struct.class.isAssignableFrom(parentStruct.getAttributes()[2].getClass()));
+    Assert.assertTrue("The attribute is not an Array", Array.class.isAssignableFrom(parentStruct.getAttributes()[6].getClass()));
+  }
+
+  @Test
+  public void canGetChildCountByParentUsingDirectJdbcPreparedStatement() throws SQLException {
+    dbSetupTracker.skipNextLaunch();
+    int childCountByParent = getChildCountByParentUsingDirectJdbcPreparedStatement(getEdbConnection());
+    Assert.assertEquals("Caesar has the wrong number of children", 3, childCountByParent);
+  }
+
+  @Test
+  public void getChildCountByParentUsingDirectJdbcWithOverload() throws Exception {
+    dbSetupTracker.skipNextLaunch();
+    int childCountByParent = getChildCountByParentUsingDirectJdbcWithOverload(getEdbConnection());
+    Assert.assertEquals("Caesar has the wrong number of children", 3, childCountByParent);
+  }
+}
--- jdbc/src/test/java/com/terradatum/jdbc/db/FunctionCallWithObjectParameterTests.java
+++ jdbc/src/test/java/com/terradatum/jdbc/db/FunctionCallWithObjectParameterTests.java
@@ -102,47 +102,4 @@ public class FunctionCallWithObjectParameterTests extends AbstractDbTest {
     BigDecimal ret = callableStatement.getBigDecimal(1);
     return ret.intValue();
   }
-
-  public int getChildCountByParentUsingDirectJdbcPreparedStatement(Connection connection) throws SQLException {
-    String commandText = "SELECT parent_child_pkg.get_child_count_by_parent_obj(?)";
-    String typeName;
-    if (OracleConnection.class.isAssignableFrom(connection.getClass()) ||
-        connection.isWrapperFor(OracleConnection.class)) {
-      typeName = "JDBC_TEST.PARENT_OBJ";
-    } else {
-      typeName = "parent_obj";
-    }
-    Struct parent = connection.createStruct(typeName, new Object[]{
-        BigDecimal.ONE, "Caesar", null, null, null, null, null
-    });
-    PreparedStatement preparedStatement = connection.prepareStatement(commandText);
-
-    preparedStatement.setObject(1, parent);
-    ResultSet resultSet = preparedStatement.executeQuery();
-    BigDecimal ret = BigDecimal.ZERO;
-    if (resultSet.next()) {
-      ret = resultSet.getBigDecimal(1);
-    }
-    return ret.intValue();
-  }
-
-  public int getChildCountByParentUsingDirectJdbcWithOverload(Connection connection) throws SQLException {
-    String commandText = "{? = call parent_child_pkg.get_child_count_by_parent(?)}";
-    String typeName;
-    if (OracleConnection.class.isAssignableFrom(connection.getClass()) ||
-        connection.isWrapperFor(OracleConnection.class)) {
-      typeName = "JDBC_TEST.PARENT_OBJ";
-    } else {
-      typeName = "parent_obj";
-    }
-    Struct parent = connection.createStruct(typeName, new Object[]{
-        BigDecimal.ONE, "Caesar", null, null, null, null, null
-    });
-    CallableStatement callableStatement = connection.prepareCall(commandText);
-    callableStatement.registerOutParameter(1, Types.NUMERIC);
-    callableStatement.setObject(2, parent);
-    callableStatement.execute();
-    BigDecimal ret = callableStatement.getBigDecimal(1);
-    return ret.intValue();
-  }
 }
